5c30ebe719a66e61980514ac1b4bd14d732444bf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-03 16:15:58 +0300

    Refactored the file for the secured endpoint

--- app/security_endpoint.py
+++ app/security_endpoint.py
@@ -2,16 +2,28 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.security import APIKeyHeader
 
-APP_TOKEN = "some_secret_api_key"
+API_TOKEN = "some_secret_api_key"
 
-security_endpoint = APIRouter("/security", tags=["Security"])
+security_endpoint = APIRouter(prefix="/security_endpoint", tags=["Security"])
 
 api_key_header = APIKeyHeader(name="Token")
 
 
 @security_endpoint.get("/")
-async def security_endpoint(token: str = Depends(api_key_header)):
+async def secured_endpoint(token: str = Depends(api_key_header)):
+    """A simple test endpoint for security authentication
+
+    Args:
+        token (str, optional): The security token. Defaults to Depends(api_key_header).
+
+    Raises:
+        HTTPException: The Data type for an HTTP error
+
+    Returns:
+        _type_: _description_
+    """
     if token != API_TOKEN:
-        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = "Invalid API Key")
-    
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid API Key")
+
     return {"Looks like": " you are authorized"}
