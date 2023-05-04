78d62823fc31a7a9c2a23aafa48e7f3fd4828959
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 10:00:31 +0300

    Reorganized the imports for theh script

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 """The entrypoint to the application"""
-from fastapi import FastAPI, status, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
 from app.security_endpoint import security_endpoint
-from models.models import UserCreate, User, UserTortoise
 from auth.password import get_password_hash
+from models.models import User, UserCreate, UserTortoise
 
 app = FastAPI()
 
@@ -48,3 +50,17 @@ async def register(user: UserCreate) -> User:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
     
     return User.from_orm(user_tortoise)
+
+
+@app.post("/token")
+async def create_token(form_data: OAuth2PasswordRequestForm = Depends(OAuth2PasswordRequestForm)):
+    email = form_data.username
+    password = form_data.password
+    user = await authenticate(email, password)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+        
+    token = await create_access_token(user)
+    
+    return {"access_token": token.access_token, "token_type": "bearer"}
