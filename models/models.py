7ac453f2eb183d84104e1d5f9844d625edb1c3d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 08:59:13 +0300

    Wrote the UserBase model anfd its Config subclass

--- models/models.py
+++ models/models.py
@@ -3,4 +3,10 @@ from pydantic import BaseModel, EmailStr
 
 
 class UserBase(BaseModel):
+    """The base model for the user"""
     email: EmailStr
+    
+    class Config:
+        """The configuration subclass for the User base model
+        """
+        orm_mode = True
