bd8f33953ae6adaa31f92e8717c8462523347bd7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 10:25:31 +0300

    Some simple refactoring

--- app/main.py
+++ app/main.py
@@ -100,4 +100,7 @@ async def get_current_user(token: str = Depends(OAuth2PasswordBearer(tokenUrl = 
 
         return cast(UserTortoise, access_token.user)
     except DoesNotExist:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+            )
