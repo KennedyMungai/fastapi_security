a889319c65018de42d18db08c0f418e6da0919a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 09:43:54 +0300

    Defined the verify password function

--- auth/password.py
+++ auth/password.py
@@ -14,3 +14,15 @@ def get_password_hash(password: str) -> str:
         str: The hashed password.
     """
     return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """The verification function
+
+    Args:
+        plain_password (str): The password provided by the user.
+        hashed_password (str): The hashed password.
+
+    Returns:
+        bool: The verification result.
+    """
+    return pwd_context.verify(plain_password, hashed_password)
