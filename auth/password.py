a7e2df75c00b7026817bea8b3196cd539e306e32
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 09:20:43 +0300

    Wrote the function that gets the password hash from the pasword provided

--- auth/password.py
+++ auth/password.py
@@ -1,1 +1,16 @@
 """The password script"""
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password: str) -> str:
+    """The hashing function 
+
+    Args:
+        password (str): The password provided by the user. 
+
+    Returns:
+        str: The hashed password.
+    """
+    return pwd_context.hash(password)
